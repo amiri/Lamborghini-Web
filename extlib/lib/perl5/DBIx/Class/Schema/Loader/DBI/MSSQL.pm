7b5a1108e6f0d2bdab884dd3f9b60b10cfc3ee8b
Amiri Barksdale at Home <amiri@metalabel.com>
2010-11-02 07:16:43 -0700

    Add some sources

---
+++ extlib/lib/perl5/DBIx/Class/Schema/Loader/DBI/MSSQL.pm
@@ -0,0 +1,310 @@
+package DBIx::Class::Schema::Loader::DBI::MSSQL;
+
+use strict;
+use warnings;
+use base 'DBIx::Class::Schema::Loader::DBI::Sybase::Common';
+use Carp::Clan qw/^DBIx::Class/;
+use mro 'c3';
+
+our $VERSION = '0.07002';
+
+=head1 NAME
+
+DBIx::Class::Schema::Loader::DBI::MSSQL - DBIx::Class::Schema::Loader::DBI MSSQL Implementation.
+
+=head1 DESCRIPTION
+
+Base driver for Microsoft SQL Server, used by
+L<DBIx::Class::Schema::Loader::DBI::Sybase::Microsoft_SQL_Server> for support
+via L<DBD::Sybase> and
+L<DBIx::Class::Schema::Loader::DBI::ODBC::Microsoft_SQL_Server> for support via
+L<DBD::ODBC>.
+
+See L<DBIx::Class::Schema::Loader> and L<DBIx::Class::Schema::Loader::Base> for
+usage information.
+
+=head1 CASE SENSITIVITY
+
+Most MSSQL databases use C<CI> (case-insensitive) collation, for this reason
+generated column names are lower-cased as this makes them easier to work with
+in L<DBIx::Class>.
+
+We attempt to detect the database collation at startup, and set the column
+lowercasing behavior accordingly, as lower-cased column names do not work on
+case-sensitive databases.
+
+To manually control case-sensitive mode, put:
+
+    preserve_case => 1|0
+
+in your Loader options.
+
+See L<preserve_case|DBIx::Class::Schema::Loader::Base/preserve_case>.
+
+B<NOTE:> this option used to be called C<case_sensitive_collation>, but has
+been renamed to a more generic option.
+
+=cut
+
+sub _setup {
+    my $self = shift;
+
+    $self->next::method(@_);
+
+    return if defined $self->preserve_case;
+
+    my $dbh = $self->schema->storage->dbh;
+
+    # We use the sys.databases query for the general case, and fallback to
+    # databasepropertyex() if for some reason sys.databases is not available,
+    # which does not work over DBD::ODBC with unixODBC+FreeTDS.
+    #
+    # XXX why does databasepropertyex() not work over DBD::ODBC ?
+    #
+    # more on collations here: http://msdn.microsoft.com/en-us/library/ms143515.aspx
+    my ($collation_name) =
+           eval { $dbh->selectrow_array('SELECT collation_name FROM sys.databases WHERE name = DB_NAME()') }
+        || eval { $dbh->selectrow_array("SELECT CAST(databasepropertyex(DB_NAME(), 'Collation') AS VARCHAR)") };
+
+    if (not $collation_name) {
+        warn <<'EOF';
+
+WARNING: MSSQL Collation detection failed. Defaulting to case-insensitive mode.
+Override the 'preserve_case' attribute in your Loader options if needed.
+
+See 'preserve_case' in
+perldoc DBIx::Class::Schema::Loader::Base
+EOF
+        $self->preserve_case(0);
+        return;
+    }
+
+    my $case_sensitive = $collation_name =~ /_(?:CS|BIN2?)(?:_|\z)/;
+
+    $self->preserve_case($case_sensitive ? 1 : 0);
+}
+
+sub _tables_list {
+    my ($self, $opts) = @_;
+
+    my $dbh = $self->schema->storage->dbh;
+    my $sth = $dbh->prepare(<<'EOF');
+SELECT t.table_name
+FROM INFORMATION_SCHEMA.TABLES t
+WHERE t.table_schema = ?
+EOF
+    $sth->execute($self->db_schema);
+
+    my @tables = map @$_, @{ $sth->fetchall_arrayref };
+
+    return $self->_filter_tables(\@tables, $opts);
+}
+
+sub _table_pk_info {
+    my ($self, $table) = @_;
+    my $dbh = $self->schema->storage->dbh;
+    my $sth = $dbh->prepare(qq{sp_pkeys '$table'});
+    $sth->execute;
+
+    my @keydata;
+
+    while (my $row = $sth->fetchrow_hashref) {
+        push @keydata, $self->_lc($row->{COLUMN_NAME});
+    }
+
+    return \@keydata;
+}
+
+sub _table_fk_info {
+    my ($self, $table) = @_;
+
+    my ($local_cols, $remote_cols, $remote_table, @rels, $sth);
+    my $dbh = $self->schema->storage->dbh;
+    eval {
+        $sth = $dbh->prepare(qq{sp_fkeys \@fktable_name = '$table'});
+        $sth->execute;
+    };
+
+    while (my $row = eval { $sth->fetchrow_hashref }) {
+        my $fk = $row->{FK_NAME};
+        push @{$local_cols->{$fk}}, $self->_lc($row->{FKCOLUMN_NAME});
+        push @{$remote_cols->{$fk}}, $self->_lc($row->{PKCOLUMN_NAME});
+        $remote_table->{$fk} = $row->{PKTABLE_NAME};
+    }
+
+    foreach my $fk (keys %$remote_table) {
+        push @rels, {
+                      local_columns => \@{$local_cols->{$fk}},
+                      remote_columns => \@{$remote_cols->{$fk}},
+                      remote_table => $remote_table->{$fk},
+                    };
+
+    }
+    return \@rels;
+}
+
+sub _table_uniq_info {
+    my ($self, $table) = @_;
+
+    my $dbh = $self->schema->storage->dbh;
+    local $dbh->{FetchHashKeyName} = 'NAME_lc';
+
+    my $sth = $dbh->prepare(qq{
+SELECT ccu.constraint_name, ccu.column_name
+FROM INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE ccu
+JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS tc on (ccu.constraint_name = tc.constraint_name)
+JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE kcu on (ccu.constraint_name = kcu.constraint_name and ccu.column_name = kcu.column_name)
+wHERE ccu.table_name = @{[ $dbh->quote($table) ]} AND constraint_type = 'UNIQUE' ORDER BY kcu.ordinal_position
+    });
+    $sth->execute;
+    my $constraints;
+    while (my $row = $sth->fetchrow_hashref) {
+        my $name = $row->{constraint_name};
+        my $col  = $self->_lc($row->{column_name});
+        push @{$constraints->{$name}}, $col;
+    }
+
+    my @uniqs = map { [ $_ => $constraints->{$_} ] } keys %$constraints;
+    return \@uniqs;
+}
+
+sub _columns_info_for {
+    my $self    = shift;
+    my ($table) = @_;
+
+    my $result = $self->next::method(@_);
+
+    while (my ($col, $info) = each %$result) {
+        my $dbh = $self->schema->storage->dbh;
+
+# find identities
+        my $sth = $dbh->prepare(qq{
+SELECT column_name 
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE columnproperty(object_id(@{[ $dbh->quote($table) ]}, 'U'), @{[ $dbh->quote($col) ]}, 'IsIdentity') = 1
+AND table_name = @{[ $dbh->quote($table) ]} AND column_name = @{[ $dbh->quote($col) ]}
+        });
+        if (eval { $sth->execute; $sth->fetchrow_array }) {
+            $info->{is_auto_increment} = 1;
+            $info->{data_type} =~ s/\s*identity//i;
+            delete $info->{size};
+        }
+
+# fix types
+        if ($info->{data_type} eq 'int') {
+            $info->{data_type} = 'integer';
+        }
+        elsif ($info->{data_type} eq 'timestamp') {
+            $info->{inflate_datetime} = 0;
+        }
+        elsif ($info->{data_type} =~ /^(?:numeric|decimal)\z/) {
+            if (ref($info->{size}) && $info->{size}[0] == 18 && $info->{size}[1] == 0) {
+                delete $info->{size};
+            }
+        }
+        elsif ($info->{data_type} eq 'float') {
+            $info->{data_type} = 'double precision';
+        }
+        elsif ($info->{data_type} =~ /^(?:small)?datetime\z/) {
+            # fixup for DBD::Sybase
+            if ($info->{default_value} && $info->{default_value} eq '3') {
+                delete $info->{default_value};
+            }
+        }
+        elsif ($info->{data_type} eq 'datetimeoffset') {
+            $info->{size} = {
+                26 => 0,
+                28 => 1,
+                29 => 2,
+                30 => 3,
+                31 => 4,
+                32 => 5,
+                33 => 6,
+                34 => 7,
+            }->{$info->{size}};
+
+            delete $info->{size} if $info->{size} == 7;
+        }
+        elsif ($info->{data_type} eq 'datetime2') {
+            $info->{size} = {
+                19 => 0,
+                21 => 1,
+                22 => 2,
+                23 => 3,
+                24 => 4,
+                25 => 5,
+                26 => 6,
+                27 => 7,
+            }->{$info->{size}};
+
+            delete $info->{size} if $info->{size} == 7;
+        }
+        elsif ($info->{data_type} eq 'time') {
+            $info->{size} = {
+                 8 => 0,
+                10 => 1,
+                11 => 2,
+                12 => 3,
+                13 => 4,
+                14 => 5,
+                15 => 6,
+                16 => 7,
+            }->{$info->{size}};
+
+            delete $info->{size} if $info->{size} == 7;
+        }
+
+        if ($info->{data_type} !~ /^(?:n?char|n?varchar|binary|varbinary|numeric|decimal|float|datetime(?:2|offset)|time)\z/) {
+            delete $info->{size};
+        }
+
+# get default
+        $sth = $dbh->prepare(qq{
+SELECT column_default
+FROM INFORMATION_SCHEMA.COLUMNS
+wHERE table_name = @{[ $dbh->quote($table) ]} AND column_name = @{[ $dbh->quote($col) ]}
+        });
+        my ($default) = eval { $sth->execute; $sth->fetchrow_array };
+
+        if (defined $default) {
+            # strip parens
+            $default =~ s/^\( (.*) \)\z/$1/x;
+
+            # Literal strings are in ''s, numbers are in ()s (in some versions of
+            # MSSQL, in others they are unquoted) everything else is a function.
+            $info->{default_value} =
+                $default =~ /^['(] (.*) [)']\z/x ? $1 :
+                    $default =~ /^\d/ ? $default : \$default;
+
+            if ((eval { lc ${ $info->{default_value} } }||'') eq 'getdate()') {
+                ${ $info->{default_value} } = 'current_timestamp';
+
+                my $getdate = 'getdate()';
+                $info->{original}{default_value} = \$getdate;
+            }
+        }
+    }
+
+    return $result;
+}
+
+=head1 SEE ALSO
+
+L<DBIx::Class::Schema::Loader::DBI::Sybase::Microsoft_SQL_Server>,
+L<DBIx::Class::Schema::Loader::DBI::ODBC::Microsoft_SQL_Server>,
+L<DBIx::Class::Schema::Loader>, L<DBIx::Class::Schema::Loader::Base>,
+L<DBIx::Class::Schema::Loader::DBI>
+
+=head1 AUTHOR
+
+See L<DBIx::Class::Schema::Loader/AUTHOR> and L<DBIx::Class::Schema::Loader/CONTRIBUTORS>.
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
+
+1;
+# vim:et sts=4 sw=4 tw=0:
---
+++ extlib/lib/perl5/MooseX/NonMoose/Meta/Role/Class.pm
@@ -0,0 +1,261 @@
+package MooseX::NonMoose::Meta::Role::Class;
+BEGIN {
+  $MooseX::NonMoose::Meta::Role::Class::VERSION = '0.15';
+}
+use Moose::Role;
+use List::MoreUtils qw(any);
+# ABSTRACT: metaclass trait for L<MooseX::NonMoose>
+
+
+has has_nonmoose_constructor => (
+    is      => 'rw',
+    isa     => 'Bool',
+    default => 0,
+);
+
+has has_nonmoose_destructor => (
+    is  => 'rw',
+    isa => 'Bool',
+    default => 0,
+);
+
+sub _determine_constructor_options {
+    my $self = shift;
+    my @options = @_;
+
+    # if we're using just the metaclass trait, but not the constructor trait,
+    # then suppress the warning about not inlining a constructor
+    my $cc_meta = Class::MOP::class_of($self->constructor_class);
+    return (@options, inline_constructor => 0)
+        unless $cc_meta->can('does_role')
+            && $cc_meta->does_role('MooseX::NonMoose::Meta::Role::Constructor');
+
+    # do nothing if extends was called, but we then added a method modifier to
+    # the constructor (this will warn, but that's okay)
+    # XXX: this is a fairly big hack, but it should cover most of the cases
+    # that actually show up in practice... it would be nice to do this properly
+    # though
+    # XXX: get constructor name from the constructor metaclass?
+    return @options
+        if $self->get_method('new')->isa('Class::MOP::Method::Wrapped');
+
+    # do nothing if we explicitly ask for the constructor to not be inlined
+    my %options = @options;
+    return @options if !$options{inline_constructor};
+
+    # otherwise, explicitly ask for the constructor to be replaced (to suppress
+    # the warning message), since this is the expected usage, and shouldn't
+    # cause a warning
+    return (replace_constructor => 1, @options);
+}
+
+sub _determine_destructor_options {
+    my $self = shift;
+    my @options = @_;
+
+    return (@options, inline_destructor => 0);
+}
+
+around _immutable_options => sub {
+    my $orig = shift;
+    my $self = shift;
+
+    my @options = $self->$orig(@_);
+
+    # do nothing if extends was never called
+    return @options if !$self->has_nonmoose_constructor
+                    && !$self->has_nonmoose_destructor;
+
+    @options = $self->_determine_constructor_options(@options);
+    @options = $self->_determine_destructor_options(@options);
+
+    return @options;
+};
+
+sub _check_superclass_constructor {
+    my $self = shift;
+
+    # if the current class defined a custom new method (since subs happen at
+    # BEGIN time), don't try to override it
+    return if $self->has_method('new');
+
+    # we need to get the non-moose constructor from the superclass
+    # of the class where this method actually exists, regardless of what class
+    # we're calling it on
+    # XXX: get constructor name from the constructor metaclass?
+    my $super_new = $self->find_next_method_by_name('new');
+
+    # if we're trying to extend a (non-immutable) moose class, just do nothing
+    return if $super_new->package_name eq 'Moose::Object';
+
+    if ($super_new->associated_metaclass->can('constructor_class')) {
+        my $constructor_class_meta = Class::MOP::Class->initialize(
+            $super_new->associated_metaclass->constructor_class
+        );
+
+        # if the constructor we're inheriting is already one of ours, there's
+        # no reason to install a new one
+        return if $constructor_class_meta->can('does_role')
+               && $constructor_class_meta->does_role('MooseX::NonMoose::Meta::Role::Constructor');
+
+        # if the constructor we're inheriting is an inlined version of the
+        # default moose constructor, don't do anything either
+        return if any { $_->isa($constructor_class_meta->name) }
+                      $super_new->associated_metaclass->_inlined_methods;
+    }
+
+    $self->add_method(new => sub {
+        my $class = shift;
+
+        my $params = $class->BUILDARGS(@_);
+        my @foreign_params = $class->can('FOREIGNBUILDARGS')
+                           ? $class->FOREIGNBUILDARGS(@_)
+                           : @_;
+        my $instance = $super_new->execute($class, @foreign_params);
+        if (!blessed($instance)) {
+            confess "The constructor for "
+                  . $super_new->associated_metaclass->name
+                  . " did not return a blessed instance";
+        }
+        elsif (!$instance->isa($class)) {
+            if (!$class->isa(blessed($instance))) {
+                confess "The constructor for "
+                      . $super_new->associated_metaclass->name
+                      . " returned an object whose class is not a parent of "
+                      . $class;
+            }
+            else {
+                bless $instance, $class;
+            }
+        }
+        return Class::MOP::Class->initialize($class)->new_object(
+            __INSTANCE__ => $instance,
+            %$params,
+        );
+    });
+    $self->has_nonmoose_constructor(1);
+}
+
+sub _check_superclass_destructor {
+    my $self = shift;
+
+    # if the current class defined a custom DESTROY method (since subs happen
+    # at BEGIN time), don't try to override it
+    return if $self->has_method('DESTROY');
+
+    # we need to get the non-moose destructor from the superclass
+    # of the class where this method actually exists, regardless of what class
+    # we're calling it on
+    my $super_DESTROY = $self->find_next_method_by_name('DESTROY');
+
+    # if we're trying to extend a (non-immutable) moose class, just do nothing
+    return if $super_DESTROY->package_name eq 'Moose::Object';
+
+    if ($super_DESTROY->associated_metaclass->can('destructor_class')
+     && $super_DESTROY->associated_metaclass->destructor_class) {
+        my $destructor_class_meta = Class::MOP::Class->initialize(
+            $super_DESTROY->associated_metaclass->destructor_class
+        );
+
+        # if the destructor we're inheriting is an inlined version of the
+        # default moose destructor, don't do anything
+        return if any { $_->isa($destructor_class_meta->name) }
+                      $super_DESTROY->associated_metaclass->_inlined_methods;
+    }
+
+    $self->add_method(DESTROY => sub {
+        my $self = shift;
+
+        local $?;
+
+        Try::Tiny::try {
+            $super_DESTROY->execute($self);
+            $self->DEMOLISHALL(Devel::GlobalDestruction::in_global_destruction);
+        }
+        Try::Tiny::catch {
+            # Without this, Perl will warn "\t(in cleanup)$@" because of some
+            # bizarre fucked-up logic deep in the internals.
+            no warnings 'misc';
+            die $_;
+        };
+
+        return;
+    });
+    $self->has_nonmoose_destructor(1);
+}
+
+around superclasses => sub {
+    my $orig = shift;
+    my $self = shift;
+
+    return $self->$orig unless @_;
+
+    my @superclasses = @_;
+    push @superclasses, 'Moose::Object'
+        unless grep { !ref($_) && $_->isa('Moose::Object') } @superclasses;
+
+    my @ret = $self->$orig(@superclasses);
+
+    $self->_check_superclass_constructor;
+    $self->_check_superclass_destructor;
+
+    return @ret;
+};
+
+no Moose::Role;
+
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+MooseX::NonMoose::Meta::Role::Class - metaclass trait for L<MooseX::NonMoose>
+
+=head1 VERSION
+
+version 0.15
+
+=head1 SYNOPSIS
+
+  package Foo;
+  use Moose -traits => 'MooseX::NonMoose::Meta::Role::Class';
+
+  # or
+
+  package My::Moose;
+  use Moose ();
+  use Moose::Exporter;
+
+  Moose::Exporter->setup_import_methods;
+  sub init_meta {
+      shift;
+      my %options = @_;
+      Moose->init_meta(%options);
+      Moose::Util::MetaRole::apply_metaclass_roles(
+          for_class       => $options{for_class},
+          metaclass_roles => ['MooseX::NonMoose::Meta::Role::Class'],
+      );
+      return Class::MOP::class_of($options{for_class});
+  }
+
+=head1 DESCRIPTION
+
+This trait implements everything involved with extending non-Moose classes,
+other than doing the actual inlining at C<make_immutable> time. See
+L<MooseX::NonMoose> for more details.
+
+=head1 AUTHOR
+
+Jesse Luehrs <doy at tozt dot net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Jesse Luehrs.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
